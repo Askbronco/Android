c624945143cd7995e2c1dbc60617f2094123bc64
Rohit Pitke <rohitpitke@Rohits-MacBook-Pro.local>
2017-03-11 10:51:23 -0800

    New changes

---
+++ app/src/main/java/com/visa/android/integration/checkoutsampleapp/app/addMerchantManually.java
@@ -0,0 +1,13 @@
+package com.visa.android.integration.checkoutsampleapp.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addMerchantManually extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_merchant_manually);
+    }
+}
